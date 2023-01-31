6e83c345c45fdf74fcc5763f3a9baeaf2da33cb6
Sarah Mattos <sahmattos13@gmail.com>
2023-01-31 17:54:41 -0300

    animação (walk, idle)

--- littlenomicon/Assets/Scripts/Player/PlayerController.cs
+++ littlenomicon/Assets/Scripts/Player/PlayerController.cs
@@ -7,6 +7,14 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
     private Vector2 move;
+    private Animator anim;
+    public Vector3 movement;
+    public Vector3 lastPos;
+
+    private void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
 
     public void OnMove(InputAction.CallbackContext context){
         move = context.ReadValue<Vector2>();
@@ -16,10 +24,23 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         movePlayer();
+        AnimatorManager();
+        
     }
     public void movePlayer(){
-        Vector3 movement = new Vector3(move.x, 0f, move.y);
+        movement = new Vector3(move.x, 0f, move.y);
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+        //transform.rotation = Quaternion.Euler(0, movement, 0);
         transform.Translate(movement * speed * Time.deltaTime, Space.World);
+    }
+    public void AnimatorManager(){
+        
+        if(transform.position != lastPos){
+            anim.SetBool("walk",true);
+        }
+        else{
+            anim.SetBool("walk",false);
+        }
+        lastPos = transform.position;
     }
 }
